9bc848c354cea564f3a91e12c4964d0eafe36127
jorgegvalencia <jorgegvalencia@gmail.com>
2023-01-25 00:13:20 +0100

    update mongo shell command

--- .docker/mongodb/initdb.d/usercreate.sh
+++ .docker/mongodb/initdb.d/usercreate.sh
@@ -1,6 +1,6 @@
 set -Eeuo pipefail
  
-mongo -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin "$MONGO_INITDB_DATABASE" <<EOF
+mongosh -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin "$MONGO_INITDB_DATABASE" <<EOF
     db.createUser({
         user: '$MONGO_INITDB_USERNAME',
         pwd: '$MONGO_INITDB_PASSWORD',
--- .docker/mongodb/scripts/mongosetup.sh
+++ .docker/mongodb/scripts/mongosetup.sh
@@ -10,7 +10,7 @@ sleep 30
 echo "done"
 
 echo SETUP.sh time now: `date +"%T" `
-mongo --host ${MONGODB1}:30001 -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} <<EOF
+mongosh --host ${MONGODB1}:30001 -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} <<EOF
 var cfg = {
     "_id": "rs0",
     "protocolVersion": 1,
